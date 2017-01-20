aba69f682886613d30032dd22be2ef1f7a7648a6
bladechapman <bechapm2@illinois.edu>
2017-01-20 15:05:42 -0600

    simplified

--- pyspy.py
+++ pyspy.py
@@ -1,6 +1,27 @@
 from functools import wraps
 import inspect
 
+#TODO: Build chained hasattr for decorator
+#TODO: Build in observation of function attributes
+
+def chained_hasattr(obj, prop_str):
+    properties = prop_str.split(".")
+    for p in properties:
+        if hasattr(obj, p):
+            obj = getattr(obj, p)
+        else:
+            return False
+    return True
+
+def chained_getattr(obj, prop_str):
+    properties = prop_str.split(".")
+    for p in properties:
+        if hasattr(obj, p):
+            obj = getattr(obj, p)
+        else:
+            raise AttributeError(obj, "does not have attribute", prop_str)
+    return obj
+
 def observe(prop_str):
     def wrap(f):
         if not hasattr(f, "__observed_attributes"):
@@ -12,55 +33,29 @@ def observe(prop_str):
 
 class PySpyBase(object):
     def __init__(self):
-        self.registered_attributes = set()
+        self.registered_attributes = dict()
 
         handler_functions = \
             ((i, j) for (i, j) in inspect.getmembers(self, inspect.ismethod) \
             if hasattr(j, "__observed_attributes") == True)
 
-        for name, handler in handler_functions:
+        for f_name, handler in handler_functions:
             for attribute_name in getattr(handler, "__observed_attributes"):
 
-                # TODO: Handle situation where observed variables have
-                # not been declared yet (so super can be moved to head)
-                value = getattr(self, attribute_name)
-                if isinstance(value, ObservableAttribute) == False:
-                    setattr(self, attribute_name, ObservableAttribute(value))
-                getattr(self, attribute_name).register_handler((name, handler))
+                if attribute_name not in self.registered_attributes:
+                    self.registered_attributes[attribute_name] = []
+                self.registered_attributes[attribute_name].append((f_name, handler))
 
-                self.registered_attributes.add(attribute_name)
-
-    # TODO: Validate / refactor these two functions
-    def __getattribute__(self, name):
-        if name == "registered_attributes" or \
-            not hasattr(self, "registered_attributes") or \
-            name not in super().__getattribute__("registered_attributes"):
-
-            return super().__getattribute__(name)
-        else:
-            return super().__getattribute__(name).__get__()
+                # # TODO: Allow default initial value to be specified in decorator
+                # if not hasattr(self, attribute_name):
+                #     setattr(self, attribute_name, None)
+                # value = getattr(self, attribute_name)
 
     def __setattr__(self, name, value):
-        # print("SET", name)
         if not hasattr(self, "registered_attributes") or \
             name not in super().__getattribute__("registered_attributes"):
             return super().__setattr__(name, value)
         else:
-            return super().__getattribute__(name).__set__(value)
-
-
-class ObservableAttribute(object):
-    def __init__(self, value):
-        self.value = value
-        self.handlers = []
-
-    def __get__(self):
-        return self.value
-
-    def __set__(self, value):
-        self.value = value
-        for i, j in self.handlers:
-            j()
-
-    def register_handler(self, handler):
-        self.handlers.append(handler)
+            for f_name, handler in super().__getattribute__("registered_attributes")[name]:
+                handler()
+            return super().__setattr__(name, value)
--- test.py
+++ test.py
@@ -1,29 +1,32 @@
 from pyspy import observe, PySpyBase
-# from pyspy import setup
+
+class Test2():
+    def __init__(self):
+        self.test_val3 = 3
 
 class Test(PySpyBase):
     def __init__(self):
-        # pass
+
         self.test_val = 1
         self.test_val2 = 2
-        self.unobserved = 10
+        self.chained_test = Test2()
         super().__init__()
 
+        # print(self.chained_test.test_val3)
+        # print(getattr(self, "chained_test.test_val3"))
+        print(getattr(getattr(self,"chained_test"), "test_val3"))
 
     @observe("test_val")
     @observe("test_val2")
+    @observe("chained_test.test_val3")
     def test(self):
-        print("HANDLER:", self.test_val, self.test_val2, self.unobserved)
-        # self.test_val2 = self.test_val2 + self.test_val
-
-    # @observe("test_val2")
-    # def test2(self):
-    #     print(self.test_val2)
+        print("HANDLER:", \
+            self.test_val, \
+            self.test_val2,
+            self.chained_test.test_val3)
 
 
-print("---INIT---")
 t = Test()
-print("---SET---")
-
 t.test_val = 3
 t.test_val2 = 5
+t.chained_test.test_val3 = 5
