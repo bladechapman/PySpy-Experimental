d72d8e4bbe5aded9958cc04bbfafe8f0fd0c4126
bladechapman <bechapm2@illinois.edu>
2017-01-27 19:25:04 -0600

    checkpoint

--- pyspy.py
+++ pyspy.py
@@ -2,7 +2,6 @@ from functools import wraps
 import inspect
 import gc
 
-#TODO: Fix ignore
 #TODO: Add ability to specify handler priority
 #TODO: Build in observation of collection attributes (dicts, arrays, ...)
 #TODO: Clean this code up a bit...
@@ -27,10 +26,12 @@ def is_handler(f):
         f = f.__func__
     return hasattr(f, "_observing")
 
-def observe(observable):
+def observe(observable, is_class=False):
     def wrap(handler):
-        if not isinstance(observable, Observable):
-            raise TypeError("Given observable must be instance of Observable")
+        if not isinstance(observable, Observable) and is_class == False:
+            raise TypeError("Given observable must be instance of Observable if not used in class")
+        elif not isinstance(observable, str) and is_class == True:
+            raise TypeError("Observable must be string reference if used in class")
 
         # retrieve the underlying function
         f = None
@@ -44,8 +45,9 @@ def observe(observable):
             f._observing = dict()
 
         # build link, no double registering
-        f._observing[observable] = {"type": "reference"}
-        observable.register_handler(handler)
+        f._observing[observable] = {"type": "reference" if isinstance(observable, Observable) else "string"}
+        if isinstance(observable, Observable):
+            observable.register_handler(handler)
 
         return handler
     return wrap
@@ -75,8 +77,59 @@ def ignore(observable):
         return handler
     return wrap
 
+def setup(init_func):
+    @wraps(init_func)
+    def modified_init_func(self, *args, **kwargs):
+        # call original init to set up class
+        init_func(self, *args, **kwargs)
 
 
+        # at this point the observed attributes have no link to their handlers
+        #   1. convert these to observable versions
+        # at this point the handlers only have string references to their targets
+        #   2. change string references to actual references
+
+
+        unmodified_handler_functions = ((i, j) for (i, j) \
+            in inspect.getmembers(self, inspect.ismethod) \
+            if is_handler(j))
+
+        # gather observed attributes
+        observed_attributes = dict()
+        for n, h in unmodified_handler_functions:
+            for t in h._observing:
+                if t not in observed_attributes:
+                    observed_attributes[t] = []
+                observed_attributes[t].append(h)
+
+        print(observed_attributes)
+
+
+        # # TODO
+        # # convert observed_attributes
+        # for a_s in observed_attributes:
+        #     # TODO: Change this to chained getattr
+        #     a = getattr(self, a_s)
+        #
+        #     o_a = None
+        #     if callable(a):
+        #         # TODO: Check to see how this works w/ bound functions
+        #         # error occurs when a is handler and a._observing still contais string references...
+        #         o_a = ObservableFunction(a) # a needs to be
+        #     else:
+        #         o_a = ObservableValue(a)
+        #
+        #     # replace normal attribute w/ observable version
+        #     setattr(self, a_s, o_a)
+        #
+        #     # change string references to actual references
+        #     for h in observed_attributes[a_s]:
+        #         del h._observing[a_s]
+        #         o_a.register_handler(h)
+        #         h._observing[o_a] = {"type": "reference"}
+
+
+    return modified_init_func
 
 
 class Observable(object):
@@ -87,6 +140,7 @@ class Observable(object):
         self._handlers.add(f)
 
 
+# TODO: Handle arguments, bound functions
 class ObservableFunction(Observable):
     def __init__(self, f):
         super().__init__()
--- test.py
+++ test.py
@@ -1,28 +1,54 @@
-from pyspy import observe, ignore, ObservableValue, ObservableFunction
+#!/usr/bin/env python3
+from pyspy import observe, ignore, setup
+from pyspy import ObservableValue, ObservableFunction
 
 
-t = ObservableValue(2)
-
-def handler():
-    print("HANDLED")
-
-handler = observe(t)(handler)
-
-def handler2():
-    print("HANDLED 2")
-
-handler = ObservableFunction(handler)
-handler2 = observe(handler)(handler2)
-
-def handler3():
-    print("HANDLED 3")
-
-handler2 = ObservableFunction(handler2)
-handler3 = observe(handler2)(handler3)
-
-print("---")
-t.set(4)
-print("---")
-
+# t = ObservableValue(2)
+#
+# def handler():
+#     print("HANDLED")
+#
+# handler = observe(t)(handler)
+#
+# def handler2():
+#     print("HANDLED 2")
+#
+# handler = ObservableFunction(handler)
+# handler2 = observe(handler)(handler2)
+#
+# def handler3():
+#     print("HANDLED 3")
+#
+# handler2 = ObservableFunction(handler2)
+# handler3 = observe(handler2)(handler3)
+#
+# print("---")
+# t.set(4)
+# print("---")
+#
 # handler = ignore(t)(handler)
 # t.set(5)
+
+
+class Test(object):
+
+    @setup
+    def __init__(self, t):
+        self.test = t
+
+    @observe("test", is_class=True)
+    def test_func(self):
+        print("test_func")
+
+    @observe("test_func", is_class=True)
+    def handler(self):
+        print("handler")
+
+
+
+
+print("---")
+t = Test(123)
+# t.test_func()
+
+# t.test.set(345)
