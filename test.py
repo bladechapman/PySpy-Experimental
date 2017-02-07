fedd02036053027e0195e2cf2718b4529ad5da08
bladechapman <bechapm2@illinois.edu>
2017-02-07 00:31:34 -0600

    removed some old comments

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-# from pyspy import observe, ignore, setup
-# from pyspy import ObservableValue, ObservableFunction, Observable
 
 import unittest
 from tests.test_basic import TestBasicUsage
